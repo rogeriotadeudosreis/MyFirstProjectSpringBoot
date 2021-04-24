105e615422831c233870759f6093092a4d6d355d
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-04-24 11:51:47 -0300

    H2 database

--- src/main/java/com/rogerioreis/myfirstprojectspringboot/entities/Category.java
+++ src/main/java/com/rogerioreis/myfirstprojectspringboot/entities/Category.java
@@ -4,6 +4,13 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
@@ -19,20 +26,24 @@ import lombok.ToString;
 @Setter
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
 
 	private String name;
 
 	@JsonIgnore
+	@OneToMany(mappedBy = "category")
 	private List<Product> products = new ArrayList<>();
 
-	public Category(long id, String name) {
-		this.id = id;
+	public Category(String name) {
 		this.name = name;
 
 	}
--- src/main/java/com/rogerioreis/myfirstprojectspringboot/entities/Product.java
+++ src/main/java/com/rogerioreis/myfirstprojectspringboot/entities/Product.java
@@ -2,6 +2,14 @@ package com.rogerioreis.myfirstprojectspringboot.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,10 +23,14 @@ import lombok.ToString;
 @Setter
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_product")
 public class Product implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
 	
@@ -26,6 +38,8 @@ public class Product implements Serializable{
 	
 	private Double price;
 	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 
 }
